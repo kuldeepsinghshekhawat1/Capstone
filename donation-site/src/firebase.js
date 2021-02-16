3901f89b4753a8a1456096ac8ab896010903b415
Krishna <mahalekrish07@gmail.com>
2021-02-16 19:38:10 +0530

    16/2/2021 first commit -KM

---
+++ donation-site/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCTWM4s_-iRAPvBB9WFPlKmjLa30JghN8g",
+    authDomain: "donation-site-5de7d.firebaseapp.com",
+    projectId: "donation-site-5de7d",
+    storageBucket: "donation-site-5de7d.appspot.com",
+    messagingSenderId: "40474398997",
+    appId: "1:40474398997:web:411b0071ab37101c8e991c",
+    measurementId: "G-9GN0LR0SB0"
+  };
---
+++ donation-site/src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.js'
+
+function header() {
+    return (
+        <div className='header'>
+            
+        </div>
+    )
+}
+
+export default header
